ea33ab93dc393c2c5bf2df5770020ebeb2e90af7
Saranya Seetharaman <Saranyas72@gmail.com>
2019-02-22 02:14:03 +0100

    fix indentation

--- src/testjson/AnalyzeJSONStreams.java
+++ src/testjson/AnalyzeJSONStreams.java
@@ -21,25 +21,25 @@ import com.google.gson.JsonParser;
 
 public class AnalyzeJSONStreams {
 	
-	public static final String URL_TO_TEST = "https://data.sfgov.org/resource/4qkm-qtkz.json";
-	List<JsonObject> items= new ArrayList<>();
+    public static final String URL_TO_TEST = "https://data.sfgov.org/resource/4qkm-qtkz.json";
+    List<JsonObject> items= new ArrayList<>();
 	
-	public AnalyzeJSONStreams() throws IOException {
-		String response = getResponseString();
+    public AnalyzeJSONStreams() throws IOException {
+	String response = getResponseString();
     	JsonParser jparse = new JsonParser();
-	    JsonArray jsonArray = (JsonArray) jparse.parse(response);
+	JsonArray jsonArray = (JsonArray) jparse.parse(response);
 	    
-	    for(int i=0;i<jsonArray.size();i++) {
-	    	items.add(jsonArray.get(i).getAsJsonObject());
-	    }
+	for(int i=0;i<jsonArray.size();i++) {
+	    items.add(jsonArray.get(i).getAsJsonObject());
 	}
+    }
 	
     @Test
     public void verifyInstallations() throws IOException {
     	
-	    List<JsonObject> result = items.stream().filter(item -> {
-	    	return item.get("start_date_year").toString().contains("1983") && item.get("end_date_year").toString().contains("1984");
-	    }).collect(Collectors.toList());
+	List<JsonObject> result = items.stream().filter(item -> {
+	    return item.get("start_date_year").toString().contains("1983") && item.get("end_date_year").toString().contains("1984");
+	}).collect(Collectors.toList());
     	
     	assertEquals(4, result.size());
 		
@@ -48,24 +48,24 @@ public class AnalyzeJSONStreams {
     @Test
     public void verifyTitleGallerySite() throws IOException {
 	    
-	    List<JsonObject> result= items.stream()
-	    		.filter(item -> {
-	    			return item.get("title").toString().contains("New Glass") && item.get("gallery_site").toString().contains("F02");
-	    		}).collect(Collectors.toList());
+	List<JsonObject> result= items.stream()
+	    .filter(item -> {
+	    	return item.get("title").toString().contains("New Glass") && item.get("gallery_site").toString().contains("F02");
+	}).collect(Collectors.toList());
 	      
-	    assertEquals(1, result.size());
+	assertEquals(1, result.size());
     }
     
     public static <E> String getResponseString() throws IOException {
-		CloseableHttpClient close = HttpClients.createDefault();
-		HttpGet get = new HttpGet(URL_TO_TEST);
-	    get.setHeader("Content-Type","application/json");
-	    CloseableHttpResponse response1 = close.execute(get);
-	    try {
-	        HttpEntity entity1 = response1.getEntity();
-	        return EntityUtils.toString(entity1, "UTF-8");
-	    } finally {
-	        response1.close();
-	    }
+	CloseableHttpClient close = HttpClients.createDefault();
+	HttpGet get = new HttpGet(URL_TO_TEST);
+	get.setHeader("Content-Type","application/json");
+	CloseableHttpResponse response1 = close.execute(get);
+	try {
+	    HttpEntity entity1 = response1.getEntity();
+	    return EntityUtils.toString(entity1, "UTF-8");
+	} finally {
+	    response1.close();
 	}
+    }
 }
